24f6fcc6833360b99054ff8e8673c4004d683f0d
Chris Atkin <atkinchris@gmail.com>
2025-02-17 21:02:56 +0000

    Get seed from query params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Board from '@/components/Board'
 import getCourse from '@/course'
+import { useSearchParams } from 'next/navigation'
 
 // Specification:
 // https://en.wikipedia.org/wiki/Geometric_Shapes_(Unicode_block)
@@ -39,14 +42,14 @@ import getCourse from '@/course'
 // 5 = ◉
 // 6 = ○
 
-interface Params {
-  seed: number
-}
+const asInt = (value: string | null) => (value ? parseInt(value, 10) : undefined)
 
-export default async function Home({ params }: { params: Promise<Params> }) {
+export default function Home() {
   const width = 16
   const height = 26
-  const seed = (await params).seed || 0
+  const searchParams = useSearchParams()
+
+  const seed = asInt(searchParams.get('seed')) || 0
   const { tiles, objects } = getCourse(width, height, seed)
 
   const shots: never[] = []
@@ -57,8 +60,4 @@ export default async function Home({ params }: { params: Promise<Params> }) {
       <Board tiles={tiles} objects={objects} shots={shots} height={height} width={width} />
     </main>
   )
-}
-
-export function generateStaticParams() {
-  return Array.from({ length: 100 }, (_, i) => ({ params: { seed: i } }))
 }
